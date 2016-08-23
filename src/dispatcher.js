9c33b0e174e36fb729c9fa754d6d7d565fd434b1
Tom McKenzie <tom@chillidonut.com>
2016-08-23 19:30:13 +1000

    Reduce actions in blocks of 100 per animationFrame

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -27,32 +27,40 @@ function getDelayedDispatch(dispatcher) {
 
   const queue = []
 
+  function drainQueue() {
+    let state = dispatcher._reduxGetState(),
+      i
+
+    for (i = 0; i < 100 && (i <= queue.length - 1); i++) {
+      // for-real dispatch the last action, triggering redux's subscribe
+      // (and thus UI re-renders). This prioritises crunching data over
+      // feedback, but potentially we should dispatch perodically, even
+      // with items in the queue
+      if (i < queue.length - 1) {
+        state = dispatcher._historyReducer(state, queue[i])
+      } else {
+        // unsure of delaying the dispatch itself is a net gain.
+        // requestAnimationFrame(
+        //   ((action) => () => dispatcher._reduxDispatch(action))(queue[i])
+        // )
+
+        dispatcher._reduxDispatch(queue[i])
+      }
+    }
+
+    // reset the queue
+    queue.splice(0, i + 1)
+
+    if (queue.length)
+      requestAnimationFrame(drainQueue)
+  }
+
   return function delayedDispatch(action) {
     queue.push(action)
 
     // on first action, queue dispatching the action queue
     if (queue.length === 1) {
-      requestAnimationFrame(() => {
-        let state = dispatcher._reduxGetState(),
-          i
-
-        for (i = 0; i <= queue.length - 1; i++) {
-          // for-real dispatch the last action, triggering redux's subscribe
-          // (and thus UI re-renders). This prioritises crunching data over
-          // feedback, but potentially we should dispatch perodically, even
-          // with items in the queue
-          if (i < queue.length - 2) {
-            state = dispatcher._historyReducer(state, queue[i])
-          } else {
-            dispatcher._reduxDispatch(queue[i])
-          }
-        }
-
-        console.log('emptying action queue of', queue.length, 'dispatched', i)
-
-        // reset the queue
-        queue.splice(0, i + 1)
-      })
+      requestAnimationFrame(drainQueue)
     }
   }
 }
